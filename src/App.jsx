778283a3777ccfe82baea9cfa687fec62b31a297
Dennis Sammy <dennissammy77@gmail.com>
2025-04-12 22:23:02 +0300

    setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css'
+import Home from './pages/Home';
+import Transactions from './pages/Transactions';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="p-4">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/transactions" element={<Transactions />} />
+        {/* <Route path="/expenses" element={<Expenses />} /> */}
+        {/* <Route path="/expenses" element={<Settings />} /> */}
+      </Routes>
+    </div>
+  )
+}
+
+export default App
