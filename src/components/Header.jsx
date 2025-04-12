59502b7e450454db8234e87219704cf0bd462353
Dennis Sammy <dennissammy77@gmail.com>
2025-04-13 00:22:47 +0300

    stle: font size udpate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Header() {
   return (
     <div className='d-flex flex-row justify-content-between align-items-center'>
-      <h1 className='custom-color-primary fw-bold'><span className='custom-color-secondary'>P</span>Wallet</h1>
-      <div className='d-flex flex-row gap-4 rounded custom-bg-primary custom-color-secondary fw-bold shadow align-items-center p-2 px-4'>
-        <a href="Dashboard">Dashboard</a>
-        <a href="Transactions">Transactions</a>
-        <a href="Settings">Settings</a>
+      <h1 className='custom-color-primary fw-bold text-xl'><span className='custom-color-secondary'>P</span>Wallet</h1>
+      <div className='d-flex flex-row gap-4 align-items-center p-2 px-4'>
+        <a href="Dashboard" className='custom-color-primary fw-bold'>Dashboard</a>
+        <a href="Transactions" className='custom-color-primary fw-bold'>Transactions</a>
+        <a href="Settings" className='custom-color-primary fw-bold'>Settings</a>
       </div>
     </div>
   )
