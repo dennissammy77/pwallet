ddfe4e77a81f08099ab5d71d298396053ac54a49
Dennis Sammy <dennissammy77@gmail.com>
2025-04-13 16:08:53 +0300

    style: transactions page responsiveness

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -45,13 +45,14 @@ export default function Transactions() {
         setTransactions(sorted);
     };
     const handleEditTransaction = (transaction)=>{
+        console.log(transaction)
         setTransactionToEdit(transaction);
         showModal()
     }
 
     return (
         <div className='py-2'>
-            <div className="d-flex flex-row justify-content-between my-2">
+            <div className="d-none flex-row justify-content-between my-2">
                 <h1 className="text-2xl font-bold text-blue-600 custom-color-dark fw-bold">Transactions</h1>
                 <div className="d-flex flex-row gap-2 align-items-center">
                     <input type="search" placeholder='search transactions' className='form-control form-control-sm' onChange={(e)=>{setsearchQuery(e.target.value)}}/>
@@ -94,65 +95,102 @@ export default function Transactions() {
                 )
             }
             {(transactions && transactions.length > 0) && (
-                <div className='table-responsive-sm my-4'>
-                    <table className="table rounded shadow table-bordered bg-white table-hover" >
-                        <thead className="">
-                            <tr className=''>
-                                <th className='w-20'>Title</th>
-                                <th className='w-20'>Description</th>
-                                <th className='w-20'>Amount</th>
-                                <th className='w-20'>Date</th>
-                                <th className='w-20'>Wallet</th>
-                                <th className='w-20'>Type</th>
-                                <th className='w-20'>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody className=''>
-                            {transactions
-                                .filter(transaction =>
-                                    transaction.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
-                                    (typeFilter === "all" || transaction.type === typeFilter) &&
-                                    (dateFilter === "all" ||
-                                        (dateFilter === "today" && moment(transaction.date).isSame(moment(), 'day')) ||
-                                        (dateFilter === "yesterday" && moment(transaction.date).isSame(moment().subtract(1, 'days'), 'day')) ||
-                                        (dateFilter === "week" && moment(transaction.date).isSame(moment(), 'week'))
+                <div className="w-100">
+                    <div className='table-responsive-sm my-4 d-none d-sm-block'>
+                        <table className="table rounded shadow table-bordered bg-white table-hover" >
+                            <thead className="">
+                                <tr className=''>
+                                    <th className='w-20'>Title</th>
+                                    <th className='w-20'>Description</th>
+                                    <th className='w-20'>Amount</th>
+                                    <th className='w-20'>Date</th>
+                                    <th className='w-20'>Wallet</th>
+                                    <th className='w-20'>Type</th>
+                                    <th className='w-20'>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody className=''>
+                                {transactions
+                                    .filter(transaction =>
+                                        transaction.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+                                        (typeFilter === "all" || transaction.type === typeFilter) &&
+                                        (dateFilter === "all" ||
+                                            (dateFilter === "today" && moment(transaction.date).isSame(moment(), 'day')) ||
+                                            (dateFilter === "yesterday" && moment(transaction.date).isSame(moment().subtract(1, 'days'), 'day')) ||
+                                            (dateFilter === "week" && moment(transaction.date).isSame(moment(), 'week'))
+                                        )
                                     )
-                                )
-                                .map((transaction)=>{
-                                return(
-                                    <tr key={transaction.id}>
-                                        <th style={{width:'200px'}}>{transaction.category}</th>
-                                        <td style={{width:'200px'}}>{transaction.name}</td>
-                                        <td>{transaction.amount}</td>
-                                        <td>{moment(transaction.date).format("MMM Do YY")}</td>
-                                        <td>{transaction.wallet}</td>
-                                        <td><span className={`badge custom-bg-${transaction.type === 'income' ? 'secondary' : 'primary' } p-1`}>{transaction.type}</span></td>
-                                        <td style={{width:'200px'}}>
-                                            <div className='d-flex flex-row gap-2'>
-                                                <button className='btn btn-sm btn-outline-dark gap-2 align-items-center' onClick={()=>handleEditTransaction(transaction)}><EditIcon/>Manage</button>
-                                                <button className='btn btn-sm btn-danger gap-2 align-items-center' onClick={()=>handleDeleteTransaction(transaction.id)}><DeleteIcon/>Delete</button>
+                                    .map((transaction)=>{
+                                    return(
+                                        <tr key={transaction.id}>
+                                            <th style={{width:'200px'}}>{transaction.category}</th>
+                                            <td style={{width:'200px'}}>{transaction.name}</td>
+                                            <td>{transaction.amount}</td>
+                                            <td>{moment(transaction.date).format("MMM Do YY")}</td>
+                                            <td>{transaction.wallet}</td>
+                                            <td><span className={`badge custom-bg-${transaction.type === 'income' ? 'secondary' : 'primary' } p-1`}>{transaction.type}</span></td>
+                                            <td style={{width:'200px'}}>
+                                                <div className='d-flex flex-row gap-2'>
+                                                    <button className='btn btn-sm btn-outline-dark gap-2 align-items-center' onClick={()=>handleEditTransaction(transaction)}><EditIcon/>Manage</button>
+                                                    <button className='btn btn-sm btn-danger gap-2 align-items-center' onClick={()=>handleDeleteTransaction(transaction.id)}><DeleteIcon/>Delete</button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                            <tfoot className='bg-white'>
+                                <tr>
+                                    <td>
+                                        <div className='d-flex flex-row gap-2 my-1 px-2'>
+                                            <button className='btn btn-sm btn-light p-2'>prev</button>
+                                            <div className='d-flex flex-row align-items-center gap-2'>
+                                                <p>1</p>
+                                                <p>of</p>
+                                                <p>2</p>
                                             </div>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                        <tfoot className='bg-white'>
-                            <tr>
-                                <td>
-                                    <div className='d-flex flex-row gap-2 my-1 px-2'>
-                                        <button className='btn btn-sm btn-light p-2'>prev</button>
-                                        <div className='d-flex flex-row align-items-center gap-2'>
-                                            <p>1</p>
-                                            <p>of</p>
-                                            <p>2</p>
+                                            <button className='btn btn-sm btn-light p-2'>next</button>
                                         </div>
-                                        <button className='btn btn-sm btn-light p-2'>next</button>
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                        {/* Cards for smaller screens */}
+                    </div>
+                    <div className="d-block d-sm-none">
+                        {transactions
+                            .filter(transaction =>
+                                transaction.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+                                (typeFilter === "all" || transaction.type === typeFilter) &&
+                                (dateFilter === "all" ||
+                                    (dateFilter === "today" && moment(transaction.date).isSame(moment(), 'day')) ||
+                                    (dateFilter === "yesterday" && moment(transaction.date).isSame(moment().subtract(1, 'days'), 'day')) ||
+                                    (dateFilter === "week" && moment(transaction.date).isSame(moment(), 'week'))
+                                )
+                            )
+                            .map((transaction) => (
+                                <div className="card mb-3" key={transaction.id}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{transaction.category}</h5>
+                                        <p className="card-text">{transaction.name}</p>
+                                        <p className="card-text"><strong>Amount:</strong> {transaction.amount}</p>
+                                        <p className="card-text"><strong>Date:</strong> {moment(transaction.date).format("MMM Do YY")}</p>
+                                        <p className="card-text"><strong>Wallet:</strong> {transaction.wallet}</p>
+                                        <span className={`badge custom-bg-${transaction.type === 'income' ? 'secondary' : 'primary'} p-1`}>
+                                            {transaction.type}
+                                        </span>
+                                        <div className="d-flex flex-row gap-2 mt-2">
+                                            <button className='btn btn-sm btn-outline-dark gap-2 align-items-center' onClick={() => handleEditTransaction(transaction)}>
+                                                <EditIcon />Manage
+                                            </button>
+                                            <button className='btn btn-sm btn-danger gap-2 align-items-center' onClick={() => handleDeleteTransaction(transaction.id)}>
+                                                <DeleteIcon />Delete
+                                            </button>
+                                        </div>
                                     </div>
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </table>
+                                </div>
+                            ))}
+                    </div>
                 </div>
                 )
             }
